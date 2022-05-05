76ae74721daa3e0ed69146c3f407cbee912b3d83
Jeff Go <j.go@entando.com>
2022-05-05 17:12:35 +0800

    ENG-3605 added unit test for complete coverage

--- test/commands/hub-group-api.test.ts
+++ test/commands/hub-group-api.test.ts
@@ -17,11 +17,18 @@ describe('hub-group-api', () => {
       expect(ctx.stdout).to.contain('51')
     })
 
-    test
+  test
     .stdout()
     .command(['hub-group-api', 'bundle', '--versionId=51'])
     .it('runs display bundle info by version id', ctx => {
       expect(ctx.stdout).to.contain('Test germano 2')
       expect(ctx.stdout).to.contain('51')
+    })
+
+  test
+    .stdout()
+    .command(['hub-group-api', 'bundle'])
+    .it('run bundle option without flags', ctx => {
+      expect(ctx.stdout).to.contain('Please provide either --name or --versionId value. Bye.')
     })
 })
