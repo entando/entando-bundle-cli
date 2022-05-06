d840b73b40e0b61f0219b928f8e6adaa3cacbd16
Jeff Go <j.go@entando.com>
2022-05-06 16:24:29 +0800

    ENG-3605 re-added nocks since it works with axios

--- test/commands/hub-group-api.test.ts
+++ test/commands/hub-group-api.test.ts
@@ -1,8 +1,32 @@
 import { expect, test } from '@oclif/test'
 
+const demoBundleOut = {
+  bundleGroupName: "Test germano 1",
+  bundleName: "{{$randomUserName}}",
+  gitSrcRepoAddress: "https://github.com/germano/mysrcrepo2.git",
+  bundleGroupVersionId: 51,
+  bundleGroupId: 49,
+  bundleId: 51,
+};
+
+const demoBundleList = [
+  { ...demoBundleOut },
+  {
+    ...demoBundleOut,
+    bundleGroupName: "Test germano 2",
+  },
+];
+
+const domainmock = 'https://63d6be0e-bd49-4c76-8fe5-195bb7cf88a5.mock.pstmn.io';
+const uri = '/ent/api/templates/bundlegroups';
+
 describe('hub-group-api', () => {
   test
     .stdout()
+    .nock(domainmock, api => api
+      .get(uri)
+      .reply(200, demoBundleList)
+    )
     .command(['hub-group-api', 'list'])
     .it('runs list option', ctx => {
       expect(ctx.stdout).to.contain('Bundle Group')
@@ -11,6 +35,10 @@ describe('hub-group-api', () => {
 
   test
     .stdout()
+    .nock(domainmock, api => api
+      .get(`${uri}?name=%22Test%20germano%201%22`)
+      .reply(200, [demoBundleOut])
+    )
     .command(['hub-group-api', 'bundle', '--name="Test germano 1"'])
     .it('runs display bundle with name filter', ctx => {
       expect(ctx.stdout).to.contain('Test germano 1')
@@ -19,9 +47,13 @@ describe('hub-group-api', () => {
 
   test
     .stdout()
+    .nock(domainmock, api => api
+      .get(`${uri}/51`)
+      .reply(200, [demoBundleOut])
+    )
     .command(['hub-group-api', 'bundle', '--versionId=51'])
     .it('runs display bundle info by version id', ctx => {
-      expect(ctx.stdout).to.contain('Test germano 2')
+      expect(ctx.stdout).to.contain('Test germano 1')
       expect(ctx.stdout).to.contain('51')
     })
 
