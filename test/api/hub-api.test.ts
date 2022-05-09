9b12ff344baea6a56240c13b3cc6050e78241f13
Jeff Go <j.go@entando.com>
2022-05-09 16:51:41 +0800

    ENG-3605 file names and class name renames

---
+++ test/api/hub-api.test.ts
@@ -0,0 +1,62 @@
+import { expect } from '@oclif/test'
+import * as nock from 'nock'
+import HubAPI from '../../src/api/hub-api'
+
+const demoBundle = {
+  bundleGroupName: "Test germano 1",
+  bundleName: "{{$randomUserName}}",
+  gitSrcRepoAddress: "https://github.com/germano/mysrcrepo2.git",
+  bundleGroupVersionId: 51,
+  bundleGroupId: 49,
+  bundleId: 51,
+}
+
+const demoBundleGroupList = [
+  {
+    bundleGroupName: "Test germano 1",
+    bundleGroupVersionId: 51,
+  },
+  {
+    bundleGroupName: "Test germano 2",
+    bundleGroupVersionId: 52,
+  },
+]
+
+const domainmock = 'https://63d6be0e-bd49-4c76-8fe5-195bb7cf88a5.mock.pstmn.io'
+const uri = '/ent/api/templates/bundlegroups'
+
+describe('Hub API', () => {
+  describe('Bundle Groups', () => {
+    const hubApi = new HubAPI()
+    it('Bundle Group List', async () => {
+      nock(domainmock)
+        .get(uri)
+        .reply(200, demoBundleGroupList)
+      const bundleGroups = await hubApi.getBundleGroups()
+      expect(bundleGroups).to.have.length(2)
+      expect(bundleGroups[0]).to.deep.equal(demoBundleGroupList[0])
+    })
+
+    it('Bundle Group List with name', async () => {
+      const [bundleGroup] = demoBundleGroupList;
+      nock(domainmock)
+        .get(`${uri}?name=Test+germano+1`)
+        .reply(200, [bundleGroup])
+      const bundleGroups = await hubApi.getBundleGroups(bundleGroup.bundleGroupName)
+      expect(bundleGroups).to.have.length(1)
+      expect(bundleGroups[0]).to.have.property('bundleGroupName', bundleGroup.bundleGroupName)
+      expect(bundleGroups[0]).to.have.property('bundleGroupVersionId', bundleGroup.bundleGroupVersionId)
+    })
+
+    it('Bundle Group Info by version id', async () => {
+      nock(domainmock)
+        .get(`${uri}/${demoBundle.bundleGroupVersionId}`)
+        .reply(200, [demoBundle])
+      const bundleGroup = await hubApi.getBundleGroupById(demoBundle.bundleGroupVersionId)
+      expect(bundleGroup).to.have.length(1)
+      expect(bundleGroup[0]).to.have.property('bundleGroupName', demoBundle.bundleGroupName)
+      expect(bundleGroup[0]).to.have.property('bundleGroupVersionId', demoBundle.bundleGroupVersionId)
+      expect(bundleGroup[0]).to.have.property('gitSrcRepoAddress', demoBundle.gitSrcRepoAddress)
+    })
+  })
+})
