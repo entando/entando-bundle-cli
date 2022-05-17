83aeda2a6e07833fd53c2e50a52493f88ab4b3c3
zonia3000 <s.zorba@entando.com>
2022-05-17 10:48:42 +0200

    ENG-3597 JSON to YAML bundle descriptor conversion

--- src/paths.ts
+++ src/paths.ts
@@ -1,4 +1,8 @@
+import * as path from 'node:path'
+
 export const RESOURCES_FOLDER = 'resources'
 export const CONFIG_FOLDER = '.ent'
 export const CONFIG_FILE = 'config.json'
 export const DEFAULT_CONFIG_FILE = 'default-config.json'
+export const OUTPUT_FOLDER = path.join(CONFIG_FOLDER, 'output')
+export const DESCRIPTORS_FOLDER = path.join(OUTPUT_FOLDER, 'descriptors')
