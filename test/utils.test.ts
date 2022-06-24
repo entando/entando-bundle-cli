4dbbb24ee942d4150321327ac6add36dc2811446
Alessio Pintus <alessio.pintus@gmail.com>
2022-06-24 16:23:12 +0200

    ENG-3865 add try and finally to restore the sandbox

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -12,17 +12,20 @@ describe('Utilities', () => {
 
   it('OutputColorWritable', () => {
     const sandbox = sinon.createSandbox()
-    const prefix = 'test'
-    const writable = new OutputColorWritable(prefix, prefix.length)
-    const stdoutStub = sandbox.stub(process.stdout, 'write')
-    const chunk1 = `first chunk `
-    const chunk2 = `second chunk${EOL} other data${EOL}`
-    const regExp1 = new RegExp(/^(.+test\s\|.+first chunk second chunk)/)
-    const regExp2 = new RegExp(/^(.+test\s\|.+other data)/)
-    writable.write(chunk1)
-    writable.write(chunk2)
-    sandbox.assert.calledWith(stdoutStub.firstCall, sandbox.match(regExp1))
-    sandbox.assert.calledWith(stdoutStub.secondCall, sandbox.match(regExp2))
-    sandbox.restore()
+    try {
+      const prefix = 'test'
+      const writable = new OutputColorWritable(prefix, prefix.length)
+      const stdoutStub = sandbox.stub(process.stdout, 'write')
+      const chunk1 = `first chunk `
+      const chunk2 = `second chunk${EOL} other data${EOL}`
+      const regExp1 = new RegExp(/^(.+test\s\|.+first chunk second chunk)/)
+      const regExp2 = new RegExp(/^(.+test\s\|.+other data)/)
+      writable.write(chunk1)
+      writable.write(chunk2)
+      sandbox.assert.calledWith(stdoutStub.firstCall, sandbox.match(regExp1))
+      sandbox.assert.calledWith(stdoutStub.secondCall, sandbox.match(regExp2))
+    } finally {
+      sandbox.restore()
+    }
   })
 })
