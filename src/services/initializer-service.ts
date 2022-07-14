aaf49341109ae45ebd194550af69c1f8fb8b5399
Jeff Go <j.go@entando.com>
2022-07-14 17:56:10 +0800

    ENG-3939 refactor FSService class

--- src/services/initializer-service.ts
+++ src/services/initializer-service.ts
@@ -1,16 +1,18 @@
 import * as fs from 'node:fs'
+import { CLIError } from '@oclif/errors'
 import { BundleDescriptorService } from './bundle-descriptor-service'
 import { debugFactory } from './debug-factory-service'
 import {
-  SVC_FOLDER,
   CONFIG_FILE,
   CONFIG_FOLDER,
   DEFAULT_CONFIG_FILE,
-  OUTPUT_FOLDER,
-  MICROFRONTENDS_FOLDER,
-  MICROSERVICES_FOLDER,
-  PSC_FOLDER,
+  OUTPUT_FOLDER
 } from '../paths'
+import {
+  ALLOWED_NAME_REGEXP,
+  INVALID_NAME_MESSAGE,
+  MAX_NAME_LENGTH
+} from '../models/bundle-descriptor-constraints'
 
 import { FSService } from './fs-service'
 import { GitService } from './git-service'
@@ -41,7 +43,7 @@ export class InitializerService {
   public async performBundleInit(): Promise<void> {
     InitializerService.debug('project scaffolding started')
 
-    this.filesys.checkBundleName()
+    this.checkBundleName()
     this.createBundleDirectories()
     this.createBundleDescriptor()
     this.createGitignore()
@@ -49,10 +51,44 @@ export class InitializerService {
     await this.git.initRepo()
   }
 
+  public checkBundleName(): void {
+    InitializerService.debug('checking if bundle name is valid')
+    const { name } = this.options
+    if (!ALLOWED_NAME_REGEXP.test(name)) {
+      throw new CLIError(
+        `'${name}' is not a valid bundle name. ${INVALID_NAME_MESSAGE}`
+      )
+    }
+
+    if (name.length > MAX_NAME_LENGTH) {
+      throw new CLIError(
+        `Bundle name is too long. The maximum length is ${MAX_NAME_LENGTH}`
+      )
+    }
+  }
+
+  public checkBundleDirectory(): void {
+    InitializerService.debug('checking bundle directory if we have access')
+
+    const { parentDirectory } = this.options
+    const bundleDir = this.filesys.getBundleDirectory()
+
+    try {
+      fs.accessSync(parentDirectory, fs.constants.W_OK)
+    } catch {
+      throw new CLIError(`Directory ${parentDirectory} is not writable`)
+    }
+
+    InitializerService.debug('checking bundle directory it exists')
+    if (fs.existsSync(bundleDir)) {
+      throw new CLIError(`Directory ${bundleDir} already exists`)
+    }
+  }
+
   private createBundleDirectories() {
     InitializerService.debug('creating bundle directories')
 
-    this.filesys.checkBundleDirectory()
+    this.checkBundleDirectory()
 
     const bundleDir = this.filesys.getBundleDirectory()
 
@@ -60,10 +96,7 @@ export class InitializerService {
     fs.mkdirSync(this.filesys.getBundleFilePath(...OUTPUT_FOLDER), {
       recursive: true
     })
-    this.filesys.createEmptySubDirectoryForGitIfNotExist(MICROSERVICES_FOLDER)
-    this.filesys.createEmptySubDirectoryForGitIfNotExist(MICROFRONTENDS_FOLDER)
-    this.filesys.createEmptySubDirectoryForGitIfNotExist(PSC_FOLDER)
-    fs.mkdirSync(this.filesys.getBundleFilePath(SVC_FOLDER))
+    this.filesys.createEmptySubdirectoriesForGitIfNotExist()
   }
 
   public async performBundleInitFromGit(
@@ -72,7 +105,7 @@ export class InitializerService {
     InitializerService.debug('cloning from bundle and creating new project')
     const { name, version } = this.options
 
-    this.filesys.checkBundleName()
+    this.checkBundleName()
     await this.git.cloneRepo(gitSrcRepoAddress)
     this.git.degit()
     await this.git.initRepo()
@@ -91,12 +124,9 @@ export class InitializerService {
   }
 
   private async createDefaultDirectories() {
-    this.filesys.createEmptySubDirectoryForGitIfNotExist(MICROSERVICES_FOLDER)
-    this.filesys.createEmptySubDirectoryForGitIfNotExist(MICROFRONTENDS_FOLDER)
-    this.filesys.createEmptySubDirectoryForGitIfNotExist(PSC_FOLDER)
+    this.filesys.createEmptySubdirectoriesForGitIfNotExist()
     this.filesys.createSubDirectoryIfNotExist(CONFIG_FOLDER)
     this.filesys.createSubDirectoryIfNotExist(...OUTPUT_FOLDER)
-    this.filesys.createSubDirectoryIfNotExist(SVC_FOLDER)
   }
 
   private createBundleDescriptor() {
