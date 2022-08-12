537a60399729239c06381aa1cdb5277c300fc809
Jeff Go <j.go@entando.com>
2022-08-12 09:57:45 +0800

    ENG-4090 added ENTANDO_CLI_DEFAULT_HUB env variable for default hub domains

--- test/helpers/mocks/hub-api.ts
+++ test/helpers/mocks/hub-api.ts
@@ -1,23 +1,22 @@
-export const mockDomain = 'https://63d6be0e-bd49-4c76-8fe5-195bb7cf88a5.mock.pstmn.io';
-export const mockUri = '/ent/api/templates/bundlegroups';
+export const mockDomain = 'https://www.entando.com/entando-hub-api'
+export const mockUri = '/ent/api/templates/bundlegroups'
 
 export const demoBundle = {
-  bundleGroupName: "Test germano 1",
+  bundleGroupName: 'Test germano 1',
   bundleName: "Germano's Test 1",
-  gitSrcRepoAddress: "https://github.com/germano/mysrcrepo2.git",
+  gitSrcRepoAddress: 'https://github.com/germano/mysrcrepo2.git',
   bundleGroupVersionId: 51,
   bundleGroupId: 49,
-  bundleId: 51,
-};
+  bundleId: 51
+}
 
 export const demoBundleGroupList = [
   {
-    bundleGroupName: "Test germano 1",
-    bundleGroupVersionId: 51,
+    bundleGroupName: 'Test germano 1',
+    bundleGroupVersionId: 51
   },
   {
-    bundleGroupName: "Test germano 2",
-    bundleGroupVersionId: 52,
-  },
-];
-
+    bundleGroupName: 'Test germano 2',
+    bundleGroupVersionId: 52
+  }
+]
