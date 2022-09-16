93d97bbf11c23e8a0561da1a6df593d1193642a8
Alessio Pintus <alessio.pintus@gmail.com>
2022-09-16 10:52:14 +0200

    ENG-4196 Default clause from the switch statement of the thumbnail status message removed

--- src/commands/pack.ts
+++ src/commands/pack.ts
@@ -181,14 +181,7 @@ export default class Pack extends BaseBuildCommand {
     const thumbnailInfo = thumbnailService.getThumbnailInfo()
 
     if (thumbnailInfo.status !== ThumbnailStatusMessage.OK) {
-      switch (thumbnailInfo.status) {
-        case ThumbnailStatusMessage.FILESIZE_EXCEEDED:
-        case ThumbnailStatusMessage.NO_THUMBNAIL:
-          this.warn(thumbnailInfo.status)
-          break
-        default:
-          this.log(color.blue(thumbnailInfo.status))
-      }
+      this.warn(thumbnailInfo.status)
     }
 
     const invalidFiles = PSCService.checkInvalidFiles()
